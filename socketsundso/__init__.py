fc840147d8e1150f62a6187b38696f0beecd7bdc
Markus Bach <git@dingensundso.de>
2022-04-17 13:18:27 +0200

    bump version to v0.0.3 and add documentation url

--- socketsundso/__init__.py
+++ socketsundso/__init__.py
@@ -1,6 +1,6 @@
 """socketsundso: A WebSocket JSON API Framework based on FastAPI, pydantic and starlette"""
 
-__version__ = "0.0.2"
+__version__ = "0.0.3"
 
 from .endpoints import WebSocketHandlingEndpoint as WebSocketHandlingEndpoint
 from .handler import on_event as on_event
