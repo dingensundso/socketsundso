99c9fff8900e298c246ccbff121b0ca8468292a6
Markus Bach <git@dingensundso.de>
2022-04-21 18:20:07 +0200

    rewrite endpoint tests

--- tests/test_endpoint.py
+++ tests/test_endpoint.py
@@ -1,3 +1,4 @@
+import pytest
 from fastapi import FastAPI
 from fastapi.testclient import TestClient
 
@@ -9,13 +10,65 @@ app = FastAPI()
 @app.websocket_route("/")
 class WSApp(WebSocketHandlingEndpoint):
     @on_event
-    async def on_echo(self, msg: str):
-        return {"msg": msg}
+    async def decorator_without_parantheses(self):
+        return {"type": "hello_world"}
+
+    @on_event()
+    async def decorator_with_parantheses(self):
+        return {"type": "hello_world"}
+
+    @on_event("decorator_with_parentheses_event")
+    async def decorator_with_parantheses_and_event(self):
+        return {"type": "hello_world"}
 
 
-def test_echo_handler():
-    client = TestClient(app)
+@WSApp.on_event
+async def class_decorator_without_parantheses():
+    return {"type": "hello_world"}
+
+
+@WSApp.on_event()
+async def class_decorator_with_parantheses():
+    return {"type": "hello_world"}
+
+
+@WSApp.on_event("class_decorator_with_parentheses_event")
+async def class_decorator_with_parantheses_and_event():
+    return {"type": "hello_world"}
+
+
+client = TestClient(app)
+
+
+@pytest.mark.parametrize(
+    "event,expected_response",
+    [
+        ("decorator_without_parantheses", {"type": "hello_world"}),
+        ("decorator_with_parantheses", {"type": "hello_world"}),
+        ("decorator_with_parentheses_event", {"type": "hello_world"}),
+        ("class_decorator_without_parantheses", {"type": "hello_world"}),
+        ("class_decorator_with_parantheses", {"type": "hello_world"}),
+        ("class_decorator_with_parentheses_event", {"type": "hello_world"}),
+    ],
+)
+def test_events(event, expected_response):
     with client.websocket_connect("/") as websocket:
-        websocket.send_json({"type": "echo", "msg": "foobar"})
+        websocket.send_json({"type": event})
         data = websocket.receive_json()
-        assert data == {"type": "echo", "msg": "foobar"}
+        assert data == expected_response
+
+
+@pytest.mark.parametrize(
+    "event",
+    [
+        ("decorator_with_parantheses_and_event"),
+        ("class_decorator_with_parantheses_and_event"),
+        ("nonexistant"),
+    ],
+)
+def test_nonexistant_events(event):
+    with client.websocket_connect("/") as websocket:
+        websocket.send_json({"type": event})
+        data = websocket.receive_json()
+        assert "errors" in data
+        assert data["errors"][0]["ctx"]["given"] == event
