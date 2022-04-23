0c992bfddab3ffb100b1a926fa5b68bd8ef9a9a7
Markus Bach <git@dingensundso.de>
2022-04-23 12:47:21 +0200

    set version number to 0.0.6.dev0

--- socketsundso/__init__.py
+++ socketsundso/__init__.py
@@ -1,6 +1,6 @@
 """socketsundso: A WebSocket JSON API Framework based on FastAPI, pydantic and starlette"""
 
-__version__ = "0.0.5"
+__version__ = "0.0.6.dev0"
 
 from .endpoints import WebSocketHandlingEndpoint as WebSocketHandlingEndpoint
 from .handler import event as event
