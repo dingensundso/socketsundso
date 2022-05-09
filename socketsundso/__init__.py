33eb15f385c1ab50d05c746fa88567869438be32
Markus Bach <git@dingensundso.de>
2022-05-09 20:50:36 +0200

    bump version number

--- socketsundso/__init__.py
+++ socketsundso/__init__.py
@@ -1,6 +1,6 @@
 """socketsundso: A WebSocket JSON API Framework based on FastAPI, pydantic and starlette"""
 
-__version__ = "0.0.6.dev1"
+__version__ = "0.0.6.dev2"
 
 from .endpoints import (
     StarletteWebSocketHandlingEndpoint as StarletteWebSocketHandlingEndpoint,
